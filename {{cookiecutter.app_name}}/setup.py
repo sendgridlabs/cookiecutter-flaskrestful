34c534d22c343730ce9e16febb8802015b14a101
Kien Pham <kien@sendgrid.com>
2013-10-11 15:57:47 -0700

    adding setup.py

---
+++ {{cookiecutter.app_name}}/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+required_packages = [
+    'Cython==0.19.1'
+    'setuptools==1.1.6',
+    'Flask==0.10.1',
+    'Flask-RESTful==0.2.3',
+    'WTForms==1.0.5',
+    'gevent==1.0dev',
+    'boto==2.10.0',
+    'Fabric==1.6.1',
+    'Pygments==1.6',
+    'Sphinx==1.2b1',
+    'WTForms==1.0.4',
+    'Werkzeug==0.9.1'
+    'airbrake-flask==0.0.3',
+    'anyjson==0.3.3',
+    'coverage==3.6',
+    'docutils==0.10',
+    'gunicorn==17.5',
+    'livereload==1.0.0',
+    'mock==1.0.1',
+    'nose==1.3.0',
+    'python-memcached==1.53',
+    'requests==1.2.3',
+    'six==1.3.0',
+    'sphinx-bootstrap-theme==0.3.1',
+    'sphinxcontrib-httpdomain==1.1.9'
+]
+
+setup(
+    name='sendgrid.apu',
+    description='Apu - List segmentation service',
+    version='0.0.1',
+    packages=['{{cookiecutter.package_namespace}}',
+              '{{cookiecutter.package_namespace}}.{{cookiecutter.app_name}}'],
+    namespace_packages=['{{cookiecutter.package_namespace}}'],
+    install_requires=required_packages,
+    dependency_links=[('git+ssh://git@github.com/surfly/gevent.git@1.0rc3#'
+            'egg=gevent-1.0dev')],
+    extras_require={
+        'test': [
+            'nose==1.3.0',
+            'ddbmock==1.0.3',
+        ]
+    }
+)
